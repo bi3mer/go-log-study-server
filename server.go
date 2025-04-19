e8581924bea2db04e9e9b3382beb729d8236a533
bi3mer <colan@comcast.net>
2025-04-19 13:33:30 -0400

    support for https kind of implemented, but really just commented out for later use

--- server.go
+++ server.go
@@ -7,11 +7,14 @@ import (
 
 const PORT = ":8080"
 
+// const PORT = ":443"
+
 func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
 	err := http.ListenAndServe(PORT, nil)
+	// err := http.ListenAndServeTLS(PORT, "server.crt", "server.key", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
