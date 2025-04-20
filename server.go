a2d62fb063f2d716ea6e08e15e9dd84014e51c82
bi3mer <colan@comcast.net>
2025-04-20 10:10:45 -0400

    post request for log receiving partially implemented, just a hello world really

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -8,8 +10,27 @@ import (
 const PORT = ":8080"
 
 // const PORT = ":443"
+func dataLogger(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	fmt.Printf("Received POST request with body: %s\n", body)
+
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, "POST request received successfully")
+}
 
 func main() {
+	http.HandleFunc("/log", dataLogger)
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
